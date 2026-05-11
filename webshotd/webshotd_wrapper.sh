23d87b1caa694e09bdf27e73b77f62492943e75e
vibecoder43 <55918427+vibecoder43@users.noreply.github.com>
2026-05-11 23:49:59 +0400

    fix(webshotd): don't unset CPU_LIMIT for webshotd

--- webshotd/webshotd_wrapper.sh
+++ webshotd/webshotd_wrapper.sh
@@ -222,5 +222,5 @@ if (( cleanup_cgroup_layout != 0 )); then
 fi
 
 script_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd -P)
-unset CPU_LIMIT DEPLOY_VCPU_LIMIT VCPU_LIMIT
+unset DEPLOY_VCPU_LIMIT VCPU_LIMIT
 exec "$script_dir/webshotd" "$@"
